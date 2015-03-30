1a77ef9e7ebaf9a162797f407ebf551470867ba3
Radhika S. Tiwari <radhikatiwari1708@gmail.com>
2015-03-30 23:39:19 +0530

    fixing issue of the multiple migrations

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,6 @@ Eventino::Application.routes.draw do
   get 'contact' => 'home#contact'
   get 'signup'  => 'users#new'
   resources :users
-  resources :participants
-
-  resources :organisers
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
